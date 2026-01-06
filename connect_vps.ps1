3f7761aad5ff651d7aa64685f2d21d4d70c01525
reyzae <reyzawirakusuma311@gmail.com>
2026-01-06 14:27:44 +0700

    chore: hide internal documentation and deployment files from public repo

--- connect_vps.ps1
+++
@@ -1,7 +0,0 @@
-﻿# Quick VPS Connect Script
-Write-Host "Connecting to VPS..." -ForegroundColor Green
-Write-Host "IP: 160.187.210.125" -ForegroundColor White
-Write-Host "User: root" -ForegroundColor White
-Write-Host ""
-
-ssh root@160.187.210.125
